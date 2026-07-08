bc9ac634eb6760e56b5d97d1c2dfe809d2ea0b33
rfbswwh5by-ux <rfbswwh5by@privaterelay.appleid.com>
2026-07-07 21:10:12 -0500

    Change price of 'Mar y Tierra' dish
    Updated the price of 'Mar y Tierra' from 24.75 to 24.25.

--- OG/src/data.js
+++ OG/src/data.js
@@ -82,7 +82,7 @@ export const fullMenu = [
       { name: "Fajitas Camarón", desc: "Shrimp fajitas.", price: "18.50" },
       { name: "Fajita Hawaiana", desc: "Sautéed onion, bell pepper, and pineapple. Choice of chicken, steak, or shrimp.", price: "15.80+" },
       { name: "Super Fajitas", desc: "Chicken, steak, chorizo, and carnitas cooked with bell peppers, tomatoes, and onions. Topped with yellow and white cheese.", price: "17.99" },
-      { name: "Mar y Tierra", desc: "Chicken, steak, shrimp, fish fillet, and chorizo.", price: "24.75" },
+      { name: "Mar y Tierra", desc: "Chicken, steak, shrimp, fish fillet, and chorizo.", price: "24.25" },
       { name: "Mi Fajita", desc: "Chicken, steak, Mexican sausage, onions, tomatoes, cilantro, and jalapeño. Not too spicy, not too mild.", price: "16.25" }
     ]
   },
